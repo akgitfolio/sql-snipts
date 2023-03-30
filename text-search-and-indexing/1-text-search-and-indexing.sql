84faf27b9eea0c5713f355a1b31a39e468359c41
ak <akv.git@gmail.com>
2023-03-30 22:27:00 +0100

    text search and indexing

---
+++ text-search-and-indexing/1-text-search-and-indexing.sql
@@ -0,0 +1,1 @@
+SELECT my_text_column FROM my_table WHERE MATCH (my_text_column) AGAINST ('search_phrase');
---
+++ text-search-and-indexing/3-text-search-and-indexing.sql
@@ -0,0 +1,1 @@
+SELECT my_text_column FROM my_table WHERE CONTAINS (my_text_column, 'play*');
