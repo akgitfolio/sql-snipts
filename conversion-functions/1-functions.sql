54948e864515f5c64399a5ae68b5f3f04745c6b8
ak <akv.git@gmail.com>
2023-06-26 20:28:00 +0100

    conversion functions

---
+++ conversion-functions/1-functions.sql
@@ -0,0 +1,29 @@
+-- Convert a number to a string
+SELECT STR(123);
+
+-- Convert a string to a number
+SELECT INT('123');
+
+-- Convert a date to a string
+SELECT DATE('2023-03-08');
+
+-- Convert a string to a date
+SELECT DATE('March 8, 2023');
+
+-- Convert a time to a string
+SELECT TIME('13:30:00');
+
+-- Convert a string to a time
+SELECT TIME('1:30 PM');
+
+-- Convert a timestamp to a string
+SELECT TIMESTAMP('2023-03-08 13:30:00');
+
+-- Convert a string to a timestamp
+SELECT TIMESTAMP('March 8, 2023 1:30 PM');
+
+-- Round a number to the nearest integer
+SELECT ROUND(123.456);
+
+-- Truncate a number to the nearest integer
+SELECT TRUNCATE(123.456);
