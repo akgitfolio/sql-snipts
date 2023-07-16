701c6d5146ec3f5570c94f7d7794d3e3fe1538d0
ak <akv.git@gmail.com>
2023-07-16 20:41:00 +0100

    database vendor

---
+++ database-vendor/1-database-vendor.sql
@@ -0,0 +1,17 @@
+-- Create a new database
+CREATE DATABASE my_database;
+
+-- Connect to the database
+\c my_database
+
+-- Create a new table
+CREATE TABLE my_table (
+  id INT NOT NULL,
+  name VARCHAR(255) NOT NULL
+);
+
+-- Insert data into the table
+INSERT INTO my_table (id, name) VALUES (1, 'John Doe');
+
+-- Query the data in the table
+SELECT * FROM my_table;
